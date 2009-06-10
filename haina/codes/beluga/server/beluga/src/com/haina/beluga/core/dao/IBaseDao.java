3b7a9567e90a8192947dbb711ee78b982e74cc5a
huangyongqiang <huangyongqiang@6c45ac76-16e4-11de-9d52-39b120432c5d>
2009-06-10 14:42:10 +0000

    所有方法的访问属性改为public

--- haina/codes/beluga/server/beluga/src/com/haina/beluga/core/dao/IBaseDao.java
+++ haina/codes/beluga/server/beluga/src/com/haina/beluga/core/dao/IBaseDao.java
@@ -6,34 +6,35 @@ import java.util.List;
 import com.haina.beluga.core.model.IModel;
 
 /**
- * The basic GenericDao interface with CRUD methods
- * Finders are added with interface inheritance and AOP introductions for concrete implementations
- *
- * Extended interfaces may declare methods starting with find... list... iterate... or scroll...
- * They will execute a preconfigured query that is looked up based on the rest of the method name
+ * The basic GenericDao interface with CRUD methods Finders are added with
+ * interface inheritance and AOP introductions for concrete implementations
+ * 
+ * Extended interfaces may declare methods starting with find... list...
+ * iterate... or scroll... They will execute a preconfigured query that is
+ * looked up based on the rest of the method name
  */
-public interface IBaseDao<T extends IModel, PK extends Serializable>
-{
+public interface IBaseDao<T extends IModel, PK extends Serializable> {
 
-    PK create(T newInstance);
+	public PK create(T newInstance);
 
-    T read(PK id);
-    //Load from cache
-    T load(PK id);
-    
-    void saveOrUpdate(T newInstance);
-    
-    void update(T transientObject);
+	public T read(PK id);
 
-    void delete(T persistentObject);
-    
-    void deleteById(PK id);
-    
-    void setBaseModel(T t);
-    
-    T getBaseModel();
-    
+	// Load from cache
+	public T load(PK id);
+
+	public void saveOrUpdate(T newInstance);
+
+	public void update(T transientObject);
+
+	public void delete(T persistentObject);
+
+	public void deleteById(PK id);
+
+	public void setBaseModel(T t);
+
+	public T getBaseModel();
+
 	public List<T> getModels(boolean useCache);
-	
+
 	public Long getModelSize();
 }
