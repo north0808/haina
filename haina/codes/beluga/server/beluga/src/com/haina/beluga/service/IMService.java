fb41000b39c996916d9d40b39dd84356aa272055
fxwarzone <fxwarzone@6c45ac76-16e4-11de-9d52-39b120432c5d>
2009-06-12 03:27:43 +0000

    增加常量类：Constant

---
+++ haina/codes/beluga/server/beluga/src/com/haina/beluga/service/IMService.java
@@ -0,0 +1,33 @@
+package com.haina.beluga.service;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import com.haina.beluga.webservice.Constant;
+
+public class IMService {
+
+	public static int getQQStatus(int qqCode){
+		if(qqCode <= 10000 || qqCode >1425000000)
+			return Constant.QQ_ARG_ERROE;
+		URL feedUrl;
+		try {
+			feedUrl = new URL("http://wpa.qq.com/pa?p=1:"+qqCode+":4");
+			java.awt.image.BufferedImage bi = javax.imageio.ImageIO.read(feedUrl);
+			if(bi.getWidth()==Constant.QQ_OFFLINE_WIDTH)
+				return Constant.QQ_OFFLINE;
+			else
+				return Constant.QQ_ONLINE;
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+			return Constant.NETWORK_ERROR;
+		} catch (IllegalArgumentException e) {
+			e.printStackTrace();
+			return Constant.NETWORK_ERROR;
+		}catch (IOException e) {
+			e.printStackTrace();
+			return Constant.NETWORK_ERROR;
+		}
+	}
+}
