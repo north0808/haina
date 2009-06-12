3d131a85c8615a3981aaa80d8d166346d25bf394
fxwarzone <fxwarzone@6c45ac76-16e4-11de-9d52-39b120432c5d>
2009-06-12 03:27:43 +0000

    增加常量类：Constant

---
+++ trunk/haina/codes/beluga/server/beluga/src/com/haina/beluga/webservice/Constant.java
@@ -0,0 +1,20 @@
+package com.haina.beluga.webservice;
+
+public class Constant {
+	
+	/*QQ不在线返回的图像宽度*/
+	public static final int QQ_OFFLINE_WIDTH = 16;
+	/*QQ参数错误*/
+	public static final int QQ_ARG_ERROE = -1;
+	/*QQ在线*/
+	public static final int QQ_ONLINE = 0;
+	/*QQ不在线*/
+	public static final int QQ_OFFLINE = 0;
+	
+	
+	/***
+	 * 系统基本异常以-10000开始.
+	 */
+	/*网络错误*/
+	public static final int NETWORK_ERROR = -10000;
+}
