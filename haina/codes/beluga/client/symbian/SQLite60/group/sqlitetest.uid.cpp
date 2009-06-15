5a005be4122ded1db2bb32681841c9c108d98cd8
shaochuan.yang <shaochuan.yang@6c45ac76-16e4-11de-9d52-39b120432c5d>
2009-06-15 12:49:22 +0000

    sqlite3 for s60v3

---
+++ haina/codes/beluga/client/symbian/SQLite60/group/sqlitetest.uid.cpp
@@ -0,0 +1,4 @@
+#include <e32cmn.h>
+#pragma data_seg(".SYMBIAN")
+__EMULATOR_IMAGE_HEADER2(0x1000007a,0x100039ce,0xa000029f,EPriorityForeground,0xBF030u,0x00000000u,0xA000029F,0,0,0)
+#pragma data_seg()
---
+++ haina/codes/beluga/client/symbian/SQLite60/sis/make.bat
@@ -0,0 +1,2 @@
+makesis sqlitetest.pkg
+signsis sqlitetest.sis \\thecore\transfer\sqlitetest.sis e:\symbian\SignSIS_356983013755076.cer e:\symbian\signsis.key 0123456
