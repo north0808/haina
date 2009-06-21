923e4178a7b6560cab4036403a39aef6a20fe786
huangyongqiang <huangyongqiang@6c45ac76-16e4-11de-9d52-39b120432c5d>
2009-06-21 06:28:32 +0000

    远程调用数据（参数、返回值）封装结构

---
+++ trunk/haina/codes/beluga/server/beluga/src/com/haina/beluga/webservice/data/AbstractRemoteParameter.java
@@ -0,0 +1,36 @@
+package com.haina.beluga.webservice.data;
+
+import java.io.Serializable;
+
+
+/**
+ * 远程调用参数的基类。<br/>
+ * @author huangyongqiang
+ * @version 1.0
+ * @since 1.0
+ * @date 2009-06-17
+ */
+public abstract class AbstractRemoteParameter extends AbstractRemoteData {
+
+	/*参数名称。*/
+	protected String name;
+	
+	/*参数值*/
+	protected Serializable value;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Serializable getValue() {
+		return value;
+	}
+
+	public void setValue(Serializable value) {
+		this.value = value;
+	}
+}
