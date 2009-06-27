d6f0cbcf7450b9b7740ecb0fe3218efeb5d15301
huangyongqiang <huangyongqiang@6c45ac76-16e4-11de-9d52-39b120432c5d>
2009-06-27 05:15:48 +0000

    实现了equals(Object object)、toString()、hashCode()方法

--- haina/codes/beluga/server/beluga/src/com/haina/beluga/domain/Weather.java
+++ haina/codes/beluga/server/beluga/src/com/haina/beluga/domain/Weather.java
@@ -3,28 +3,30 @@ package com.haina.beluga.domain;
 import org.springframework.stereotype.Component;
 
 import com.haina.beluga.core.model.VersionalModel;
+import org.apache.commons.lang.builder.EqualsBuilder;
+import org.apache.commons.lang.builder.HashCodeBuilder;
+import org.apache.commons.lang.builder.ToStringBuilder;
+
 /**
-*
+ * 
  * @hibernate.class table="Weather"
  * @hibernate.cache usage="read-write"
  */
- @Component
-public class Weather extends VersionalModel{
+@Component
+public class Weather extends VersionalModel {
 
-	
 	private static final long serialVersionUID = -326286128859607526L;
-	private String date;/*天气日期*/ 
-	private String weatherCityCode; 
-	private String weatherType; 
+	private String date;/* 天气日期 */
+	private String weatherCityCode;
+	private String weatherType;
 	private boolean isNight;
-	private int high; 
-	private int low; 
+	private int high;
+	private int low;
 	private String wind;
 	private String icon;
-	private String issuetime;/*发布时间*/
+	private String issuetime;/* 发布时间 */
 
-	
-	 /**
+	/**
 	 * @hibernate.property column="date" length="20"
 	 * @return String
 	 */
@@ -35,7 +37,8 @@ public class Weather extends VersionalModel{
 	public void setDate(String date) {
 		this.date = date;
 	}
-	 /**
+
+	/**
 	 * @hibernate.property column="weatherCityCode" length="20"
 	 * @return String
 	 */
@@ -46,6 +49,7 @@ public class Weather extends VersionalModel{
 	public void setWeatherCityCode(String weatherCityCode) {
 		this.weatherCityCode = weatherCityCode;
 	}
+
 	/**
 	 * @hibernate.property column="weatherType" length="50"
 	 * @return String
@@ -57,6 +61,7 @@ public class Weather extends VersionalModel{
 	public void setWeatherType(String weatherType) {
 		this.weatherType = weatherType;
 	}
+
 	/**
 	 * @hibernate.property column="high"
 	 * @return int
@@ -68,6 +73,7 @@ public class Weather extends VersionalModel{
 	public void setHigh(int high) {
 		this.high = high;
 	}
+
 	/**
 	 * @hibernate.property column="low"
 	 * @return int
@@ -79,7 +85,8 @@ public class Weather extends VersionalModel{
 	public void setLow(int low) {
 		this.low = low;
 	}
-	 /**
+
+	/**
 	 * @hibernate.property column="wind" length="30"
 	 * @return String
 	 */
@@ -90,7 +97,8 @@ public class Weather extends VersionalModel{
 	public void setWind(String wind) {
 		this.wind = wind;
 	}
-	 /**
+
+	/**
 	 * @hibernate.property column="icon" length="30"
 	 * @return String
 	 */
@@ -101,8 +109,9 @@ public class Weather extends VersionalModel{
 	public void setIcon(String icon) {
 		this.icon = icon;
 	}
+
 	/**
-	 * @hibernate.property column="isNight" 
+	 * @hibernate.property column="isNight"
 	 * @return boolean
 	 */
 	public boolean isNight() {
@@ -112,6 +121,7 @@ public class Weather extends VersionalModel{
 	public void setNight(boolean isNight) {
 		this.isNight = isNight;
 	}
+
 	/**
 	 * @hibernate.property column="issuetime" length="10"
 	 * @return String
@@ -133,32 +143,53 @@ public class Weather extends VersionalModel{
 		// TODO Auto-generated method stub
 		return version;
 	}
-	
-	@Override
-	public boolean equals(Object object) {
-		// TODO Auto-generated method stub
-		return false;
-	}
+
 	/**
-	 * @hibernate.id column="ID" generator-class="uuid"  unsaved-value="null"
+	 * @hibernate.id column="ID" generator-class="uuid" unsaved-value="null"
 	 * @return String
 	 */
 	@Override
 	public String getId() {
-		// TODO Auto-generated method stub
 		return id;
 	}
 
-	@Override
-	public int hashCode() {
-		// TODO Auto-generated method stub
-		return 0;
+	/**
+	 * @see java.lang.Object#equals(Object)
+	 */
+	public boolean equals(Object object) {
+		if (!(object instanceof Weather)) {
+			return false;
+		}
+		Weather rhs = (Weather) object;
+		return new EqualsBuilder().append(this.icon, rhs.icon).append(
+				this.wind, rhs.wind).append(this.isNight, rhs.isNight).append(
+				this.weatherCityCode, rhs.weatherCityCode).append(this.high,
+				rhs.high).append(this.weatherType, rhs.weatherType).append(
+				this.issuetime, rhs.issuetime).append(this.low, rhs.low)
+				.append(this.date, rhs.date).isEquals();
 	}
 
-	@Override
+	/**
+	 * @see java.lang.Object#hashCode()
+	 */
+	public int hashCode() {
+		return new HashCodeBuilder(1239287031, 192974521).append(this.icon)
+				.append(this.wind).append(this.isNight).append(
+						this.weatherCityCode).append(this.high).append(
+						this.weatherType).append(this.issuetime).append(
+						this.low).append(this.date).toHashCode();
+	}
+
+	/**
+	 * @see java.lang.Object#toString()
+	 */
 	public String toString() {
-		// TODO Auto-generated method stub
-		return null;
+		return new ToStringBuilder(this).append("weatherCityCode",
+				this.weatherCityCode).append("icon", this.icon).append(
+				"issuetime", this.issuetime).append("low", this.low).append(
+				"date", this.date).append("wind", this.wind).append("night",
+				this.isNight()).append("weatherType", this.weatherType).append(
+				"high", this.high).append("id", this.getId()).toString();
 	}
 
 }
