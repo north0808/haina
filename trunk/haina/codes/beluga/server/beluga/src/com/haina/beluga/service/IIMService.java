92c12b623ac6f5935b49a743472a5054f5eaafb3
fxwarzone <fxwarzone@6c45ac76-16e4-11de-9d52-39b120432c5d>
2009-06-27 04:22:24 +0000

    xg

---
+++ trunk/haina/codes/beluga/server/beluga/src/com/haina/beluga/service/IIMService.java
@@ -0,0 +1,9 @@
+package com.haina.beluga.service;
+
+import com.haina.beluga.webservice.data.hessian.HessianRemoteReturning;
+
+public interface IIMService {
+	
+	 public HessianRemoteReturning getQQStatus(int qqCode);
+
+}
