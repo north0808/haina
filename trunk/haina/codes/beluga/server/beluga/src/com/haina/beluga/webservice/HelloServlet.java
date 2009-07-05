f521a20633b8997ba0f04a4c5cf1acf10f8ca9c7
huangyongqiang <huangyongqiang@6c45ac76-16e4-11de-9d52-39b120432c5d>
2009-07-05 08:46:14 +0000

    增加了spring的测试方法

--- trunk/haina/codes/beluga/server/beluga/src/com/haina/beluga/webservice/HelloServlet.java
+++ trunk/haina/codes/beluga/server/beluga/src/com/haina/beluga/webservice/HelloServlet.java
@@ -1,4 +1,5 @@
 package com.haina.beluga.webservice;
+
 // Filename: HelloServlet.java
 // Compile: javac HelloServlet.java
 import java.io.IOException;
@@ -11,50 +12,55 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
 import com.caucho.hessian.micro.MicroHessianInput;
 import com.caucho.hessian.micro.MicroHessianOutput;
+import com.haina.beluga.util.SpringContext;
 
 public class HelloServlet extends HttpServlet {
-    /**
+	/**
 	 * 
 	 */
 	private static final long serialVersionUID = -5774807377339779327L;
 
-
 	public void doGet(HttpServletRequest request, HttpServletResponse res)
-                    throws ServletException, IOException {
-		 res.setStatus(500);
-	      PrintWriter out = res.getWriter();
-	      res.setContentType("text/html");
-	      out.println("<h1>UUCenter - Requires POST</h1>");
-	      return;
+			throws ServletException, IOException {
+		testSpring();
+		res.setStatus(500);
+		PrintWriter out = res.getWriter();
+		res.setContentType("text/html");
+		out.println("<h1>UUCenter - Requires POST</h1>");
+		return;
 	}
 
-
-	public void doPost(HttpServletRequest request, HttpServletResponse response) 
-					throws ServletException, IOException {
-					
+	public void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		testSpring();
 		InputStream is = request.getInputStream();
 		OutputStream os = response.getOutputStream();
-		
-		//��ȡ�ӿͻ��˴���4�����
+
+		// ��ȡ�ӿͻ��˴���4�����
 		MicroHessianInput hessianInput = new MicroHessianInput(is);
 		String user = hessianInput.readString();
 		String password = hessianInput.readString();
-		
-		
-		//��������ͻ���
+
+		// ��������ͻ���
 		MicroHessianOutput hessianOutput = new MicroHessianOutput(os);
-		if(user.compareTo("admin") == 0 && password.compareTo("123456789") == 0)
-		{
+		if (user.compareTo("admin") == 0
+				&& password.compareTo("123456789") == 0) {
 			hessianOutput.writeBoolean(true);
 			hessianOutput.writeString("suc");
-		}
-		else
-		{
+		} else {
 			hessianOutput.writeBoolean(false);
 			hessianOutput.writeString("fail");
 		}
 	}
-}
 
+	public void testSpring() {
+		SpringContext springContext = SpringContext.getInstance();
+		DriverManagerDataSource dataSource = (DriverManagerDataSource) springContext
+				.getBean("dataSource");
+		System.out.println(dataSource.getUrl());
+	}
+}
