205344799b2d294478aa129b8047664c3ac5096f
huangyongqiang <huangyongqiang@6c45ac76-16e4-11de-9d52-39b120432c5d>
2009-07-05 09:29:21 +0000

    新增认证鉴权接口

---
+++ haina/codes/beluga/server/beluga/src/com/haina/beluga/webservice/IAuthenticationService.java
@@ -0,0 +1,12 @@
+package com.haina.beluga.webservice;
+
+/**
+ * 认证业务处理接口。<br/>
+ * @author huangyongqiang
+ * @version 1.0
+ * @since 1.0
+ * @date 2009-07-05
+ */
+public interface IAuthenticationService {
+
+}
