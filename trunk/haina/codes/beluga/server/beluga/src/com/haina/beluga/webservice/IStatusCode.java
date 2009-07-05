54c650a9382a96657bb720a09186ded1a0579408
huangyongqiang <huangyongqiang@6c45ac76-16e4-11de-9d52-39b120432c5d>
2009-07-05 14:19:30 +0000

    新增

---
+++ trunk/haina/codes/beluga/server/beluga/src/com/haina/beluga/webservice/IStatusCode.java
@@ -0,0 +1,15 @@
+package com.haina.beluga.webservice;
+
+/**
+ * 状态码常量接口。<br/>
+ * @author huangyongqiang
+ * @version 1.0
+ * @since 1.0
+ * @date 2009-06-21
+ */
+public interface IStatusCode {
+
+	Integer SUCCESS=1000;
+	
+	Integer FAILURE=SUCCESS+1;
+}
