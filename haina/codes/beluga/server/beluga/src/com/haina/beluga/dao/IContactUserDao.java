d11c8ecc0e8a9575f14abc2d6fe75430eb46e56c
huangyongqiang <huangyongqiang@6c45ac76-16e4-11de-9d52-39b120432c5d>
2009-07-09 11:34:21 +0000

    增加了方法

--- haina/codes/beluga/server/beluga/src/com/haina/beluga/dao/IContactUserDao.java
+++ haina/codes/beluga/server/beluga/src/com/haina/beluga/dao/IContactUserDao.java
@@ -1,11 +1,8 @@
 package com.haina.beluga.dao;
 
-import java.util.Set;
 
 import com.haina.beluga.core.dao.IBaseDao;
 import com.haina.beluga.domain.ContactUser;
-import com.haina.beluga.domain.UserProfile;
-import com.haina.beluga.domain.UserProfileExt;
 
 /**
  * 联系人用户领域模型类Dao访问接口。<br/>
@@ -24,31 +21,9 @@ public interface IContactUserDao extends IBaseDao<ContactUser,String> {
 	public ContactUser getContactUserByLoginName(String loginName);
 	
 	/**
-	 * 通过登录名得到用户详细信息。<br/>
-	 * @param loginName
+	 * 通过手机号码得到用户。<br/>
+	 * @param mobile
 	 * @return
 	 */
-	public UserProfile getUserProfileByLoginName(String loginName);
-	
-	/**
-	 * 通过登录名得到用户详细扩展信息。<br/>
-	 * @param loginName
-	 * @return
-	 */
-	public Set<UserProfileExt> getUserProfileExtByLoginName(String loginName);
-	
-	/**
-	 * 通过用户Id得到用户详细信息。<br/>
-	 * @param id
-	 * @return
-	 */
-	public UserProfile getUserProfileById(String id);
-	
-	/**
-	 * 通过用户Id得到用户详细扩展信息。<br/>
-	 * @param id
-	 * @return
-	 */
-	public Set<UserProfileExt> getUserProfileExtById(String id);
-
+	public ContactUser getContactUserByMobile(String mobile);
 }
