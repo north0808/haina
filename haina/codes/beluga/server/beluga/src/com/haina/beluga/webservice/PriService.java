15d174657bd9abb0e403ef8c1de9c9c1bd5133b1
huangyongqiang <huangyongqiang@6c45ac76-16e4-11de-9d52-39b120432c5d>
2009-07-09 11:37:06 +0000

    修改了各个方法的返回值

--- haina/codes/beluga/server/beluga/src/com/haina/beluga/webservice/PriService.java
+++ haina/codes/beluga/server/beluga/src/com/haina/beluga/webservice/PriService.java
@@ -1,6 +1,16 @@
 package com.haina.beluga.webservice;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.haina.beluga.core.util.StringUtils;
+import com.haina.beluga.domain.ContactUser;
+import com.haina.beluga.service.IContactUserService;
+import com.haina.beluga.service.ILocaleMessageService;
+import com.haina.beluga.webservice.data.hessian.HessianRemoteReturning;
 
 /**
  * 个人服务Api接口实现类。<br/>
@@ -14,43 +24,84 @@ import org.springframework.stereotype.Component;
 public class PriService implements IPriService {
 	
 	private static final long serialVersionUID = 4943053100272442973L;
+	
+	@Autowired(required=true)
+	private AuthenticationService authenticationService;
+	
+	@Autowired(required=true)
+	private IContactUserService contactUserService;
+	
+	@Autowired(required=true)
+	private ILocaleMessageService localeMessageService;
 
 	@Override
-	public void editLoginName(String email, String neoEmail) {
-		// TODO Auto-generated method stub
+	public HessianRemoteReturning editLoginName(String email, String neoEmail) {
+		HessianRemoteReturning ret = new HessianRemoteReturning();
 		
+		return ret;
 	}
 
 	@Override
-	public void editMobile(String email, String oldMobile, String neoMobile) {
-		// TODO Auto-generated method stub
+	public HessianRemoteReturning editMobile(String email, String oldMobile, String neoMobile) {
+		HessianRemoteReturning ret = new HessianRemoteReturning();
 		
+		return ret;
 	}
 
 	@Override
-	public void editPwd(String email, String oldPwd, String neoPwd) {
-		// TODO Auto-generated method stub
+	public HessianRemoteReturning editPwd(String email, String oldPwd, String neoPwd) {
+		HessianRemoteReturning ret = new HessianRemoteReturning();
 		
+		return ret;
 	}
 
 	@Override
-	public void login(String email, String pwd, String srcAppCode,
+	@Transactional(propagation=Propagation.REQUIRED, readOnly=false,
+			isolation=Isolation.READ_COMMITTED,rollbackFor = Exception.class)
+	public HessianRemoteReturning login(String email, String pwd, String srcAppCode,
 			String destAppCode, String userLoginIp) {
-		// TODO Auto-generated method stub
+		HessianRemoteReturning ret = new HessianRemoteReturning();
 		
+		return ret;
 	}
 
 	@Override
-	public void logout(String email) {
-		// TODO Auto-generated method stub
+	public HessianRemoteReturning logout(String email) {
+		HessianRemoteReturning ret = new HessianRemoteReturning();
 		
+		return ret;
 	}
 
 	@Override
-	public void register(String email, String password, String mobile,
-			String description, String srcAppCode, String registerIp) {
-		// TODO Auto-generated method stub
+	@Transactional(propagation=Propagation.REQUIRED, readOnly=false,
+			isolation=Isolation.READ_COMMITTED,rollbackFor = Exception.class)
+	public HessianRemoteReturning register(String email, String password, String mobile,
+			String description, String registerIp,String lang) {
+		HessianRemoteReturning ret = new HessianRemoteReturning();
+		//$1 验证合法性
+		if(StringUtils.isNull(email) || StringUtils.isNull(password)) {
+			ret.setStatusCode(IStatusCode.FAILURE);
+			ret.setStatusText(localeMessageService
+					.getI18NMessage("com.haina.shield.message.passportuser.register.failure.email.and.passpord"));
+			return ret;
+		}
+		if(StringUtils.isNull(mobile)) {
+			ret.setStatusCode(IStatusCode.FAILURE);
+			ret.setStatusText(localeMessageService
+					.getI18NMessage("com.haina.shield.message.passportuser.register.failure.mobile"));
+			return ret;
+		}
+		ContactUser contactUser=contactUserService.addContactUser(email, password, mobile);
+		if(null!=contactUser && !contactUser.getMobile().equals(mobile.trim())) {
+			ret.setStatusCode(IStatusCode.FAILURE);
+			ret.setStatusText(localeMessageService
+					.getI18NMessage("com.haina.shield.message.passportuser.register.failure.existent.email.or.mobile"));
+			return ret;
+		}
 		
+		//$2 向认证中心注册
+		ret.setStatusCode(IStatusCode.SUCCESS);
+		return ret;
 	}
 
 }
