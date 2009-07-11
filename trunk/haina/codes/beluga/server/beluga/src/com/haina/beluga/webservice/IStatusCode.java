9a5997919053f6ff4c93d43d47843d0c65266856
huangyongqiang <huangyongqiang@6c45ac76-16e4-11de-9d52-39b120432c5d>
2009-07-11 16:44:40 +0000

    增加了状态码

--- trunk/haina/codes/beluga/server/beluga/src/com/haina/beluga/webservice/IStatusCode.java
+++ trunk/haina/codes/beluga/server/beluga/src/com/haina/beluga/webservice/IStatusCode.java
@@ -9,7 +9,38 @@ package com.haina.beluga.webservice;
  */
 public interface IStatusCode {
 
+	/**
+	 * 成功。<br/>
+	 */
 	Integer SUCCESS=1000;
 	
-	Integer FAILURE=SUCCESS+1;
+	/**
+	 * 未知错误。<br/>
+	 */
+	Integer UNKNOW_ERROR=SUCCESS+1;
+	
+	/**
+	 * 网络错误。<br/>
+	 */
+	Integer NETWORK_ERROR=UNKNOW_ERROR+1;
+	
+	/**
+	 * 电子邮件或密码不符合要求。<br/>
+	 */
+	Integer EMAIL_PASSWORD_INVALID=NETWORK_ERROR+1;
+	
+	/**
+	 * 手机号码不符合要求。<br/>
+	 */
+	Integer MOBILE_INVALID=EMAIL_PASSWORD_INVALID+1;
+	
+	/**
+	 * 用户已存在。<br/>
+	 */
+	Integer CONTACT_USER_EXISTENT=MOBILE_INVALID+1;
+	
+	/**
+	 * 注册成功，但暂时不能生成登录护照。<br/>
+	 */
+	Integer REGISTER_SUCCESS_PASSPORT_FAILD=CONTACT_USER_EXISTENT+1;
 }
