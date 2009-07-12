f53af97d15f84ca805ca07e0134e849ac7d709f8
huangyongqiang <huangyongqiang@6c45ac76-16e4-11de-9d52-39b120432c5d>
2009-07-12 04:50:18 +0000

    新增

---
+++ haina/codes/beluga/server/beluga/src/com/haina/beluga/service/IPassportService.java
@@ -0,0 +1,79 @@
+package com.haina.beluga.service;
+
+import org.springframework.beans.factory.InitializingBean;
+
+import com.haina.beluga.domain.ContactUser;
+import com.haina.beluga.dto.PassportDto;
+
+/**
+ * 用户验证护照业务处理接口。<br/>
+ * @author huangyongqiang
+ * @version 1.0
+ * @since 1.0
+ * @data 2009-07-11
+ *
+ */
+public interface IPassportService extends InitializingBean {
+
+	/**
+	 * 添加护照。<br/>
+	 * @param contactUser 联系人用户
+	 * @return
+	 */
+	public PassportDto addPassport(ContactUser contactUser);
+	
+	/**
+	 * 更新护照。<br/>
+	 * @param loginName 登录名
+	 * @return
+	 */
+	public PassportDto updatePassport(String loginName);
+	
+	/**
+	 * 取得护照。<br/>
+	 * @param loginName 登录名
+	 * @return
+	 */
+	public PassportDto getPassport(String loginName);
+	
+	/**
+	 * 是否过期的护照。<br/>
+	 * @param loginName 登录名
+	 * @return
+	 */
+	public boolean isExpiredPassport(String loginName);
+	
+	/**
+	 * 是否过期的护照。<br/>
+	 * @param passportDto 护照对象
+	 * @return
+	 */
+	public boolean isExpiredPassport(PassportDto passportDto); 
+	
+	/**
+	 * 删除单个护照。<br/>
+	 * @param loginName 登录名
+	 * @return
+	 */
+	public boolean removePassport(String loginName);
+	
+	/**
+	 * 删除所有护照。<br/>
+	 * @return
+	 */
+	public boolean removeAllPassport();
+	
+	/**
+	 * 是否过期的登录。<br/>
+	 * @param loginName 登录名
+	 * @return
+	 */
+	public boolean isExpiredLogin(String loginName);
+	
+	/**
+	 * 使登录失效。<br/>
+	 * @param loginName
+	 * @return
+	 */
+	public boolean expireLogin(String loginName);
+}
