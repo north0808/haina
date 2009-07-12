8caa667c887087156927d1d1bbee0d9c361ed774
huangyongqiang <huangyongqiang@6c45ac76-16e4-11de-9d52-39b120432c5d>
2009-07-12 04:51:45 +0000

    实现类注册、登录、退出功能

--- haina/codes/beluga/server/beluga/src/com/haina/beluga/webservice/IPriService.java
+++ haina/codes/beluga/server/beluga/src/com/haina/beluga/webservice/IPriService.java
@@ -16,25 +16,25 @@ public interface IPriService extends Serializable {
 	
 	/**
 	 * 联系人用户登录。<br/>
-	 * @param email 用户的登录名
-	 * @param pwd 密码
+	 * @param loginName 用户的登录名
+	 * @param password 密码
 	 * @param srcAppCode 来源认证应用代码
 	 * @param destAppCode 目标认证应用代码
 	 * @param userLoginIp 用户登录的IP地址
 	 */
-	public HessianRemoteReturning login(String email, String pwd,String srcAppCode, String destAppCode, String userLoginIp);
+	public HessianRemoteReturning login(String loginName, String password,String srcAppCode, String destAppCode, String userLoginIp);
 	
 	/**
 	 * 联系人用户注册。<br/>
 	 * 注册成功后的用户自动登录。
-	 * @param email 用户的登录名
+	 * @param loginName 用户的登录名
 	 * @param password 密码
 	 * @param mobile 移动电话号码
 	 * @param description 描述
 	 * @param registerIp 注册用户的IP地址
 	 * @param lang 语言环境
 	 */
-	public HessianRemoteReturning register(String email, String password, String mobile, String description,
+	public HessianRemoteReturning register(String loginName, String password, String mobile, String description,
 			String registerIp,String lang);
 	
 	/**
@@ -46,20 +46,20 @@ public interface IPriService extends Serializable {
 	/**
 	 * 修改密码。<br/>
 	 * 
-	 * @param email 用户的登录名
+	 * @param loginName 用户的登录名
 	 * @param oldPwd 旧密码
 	 * @param neoPwd 新密码
 	 */
-	public HessianRemoteReturning editPwd(String email, String oldPwd, String neoPwd);
+	public HessianRemoteReturning editPwd(String loginName, String oldPwd, String neoPwd);
 
 	/**
 	 * 修改手机号码。<br/>
 	 * 
-	 * @param email 用户的登录名
+	 * @param loginName 用户的登录名
 	 * @param oldMobile 旧手机号码
 	 * @param neoMobile 新手机号码
 	 */
-	public HessianRemoteReturning editMobile(String email, String oldMobile, String neoMobile);
+	public HessianRemoteReturning editMobile(String loginName, String oldMobile, String neoMobile);
 	
 	/**
 	 * 修改登录名（同时修改电子邮件）。<br/>
