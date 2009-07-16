1def5c97c8959d55b657433163f6655467bb438f
fxwarzone <fxwarzone@6c45ac76-16e4-11de-9d52-39b120432c5d>
2009-07-16 09:49:42 +0000

    修改状态码

--- trunk/haina/codes/beluga/server/beluga/src/com/haina/beluga/webservice/IStatusCode.java
+++ trunk/haina/codes/beluga/server/beluga/src/com/haina/beluga/webservice/IStatusCode.java
@@ -22,65 +22,65 @@ public interface IStatusCode {
 	/**
 	 * 网络错误。<br/>
 	 */
-	int NETWORK_ERROR=UNKNOW_ERROR+1;
+	int NETWORK_ERROR=1001;
 	
 	/**
 	 * 登录名或密码无效。<br/>
 	 */
-	int LOGINNAME_PASSWORD_INVALID=NETWORK_ERROR+1;
+	int LOGINNAME_PASSWORD_INVALID=1002;
 	
 	/**
 	 * 手机号码无效。<br/>
 	 */
-	int MOBILE_INVALID=LOGINNAME_PASSWORD_INVALID+1;
+	int MOBILE_INVALID=1003;
 	
 	/**
 	 * 用户已存在。<br/>
 	 */
-	int CONTACT_USER_EXISTENT=MOBILE_INVALID+1;
+	int CONTACT_USER_EXISTENT=1004;
 	
 	/**
 	 * 注册成功，但暂时不能生成登录护照。<br/>
 	 */
-	int REGISTER_SUCCESS_PASSPORT_FAILD=CONTACT_USER_EXISTENT+1;
+	int REGISTER_SUCCESS_PASSPORT_FAILD=1005;
 	
 	/**
 	 * 登录失败，暂时不能生成登录护照。<br/>
 	 */
-	int LOGIN_PASSPORT_FAILD=REGISTER_SUCCESS_PASSPORT_FAILD+1;
+	int LOGIN_PASSPORT_FAILD=1006;
 	
 	/**
 	 * 用户不存在或用户未激活。<br/>
 	 */
-	int INVALID_CONTACT_USER=LOGIN_PASSPORT_FAILD+1;
+	int INVALID_CONTACT_USER=1007;
 	
 	/**
 	 * 登录名或密码无效。<br/>
 	 */
-	int LOGINNAME_OR_PASSWORD_INVALID=INVALID_CONTACT_USER+1;
+	int LOGINNAME_OR_PASSWORD_INVALID=1008;
 	
 	/**
 	 * 登录护照无效或已过期。<br/>
 	 */
-	int INVALID_LOGIN_PASSPORT=LOGINNAME_OR_PASSWORD_INVALID+1;
+	int INVALID_LOGIN_PASSPORT=1009;
 	
 	/**
 	 * 无效的电子邮件地址。<br/>
 	 */
-	int INVALID_EMAIL=INVALID_LOGIN_PASSPORT+1;
+	int INVALID_EMAIL=1010;
 	
 	/**
 	 * 无效的电子邮件地址。<br/>
 	 */
-	int INVALID_MOBILE=INVALID_EMAIL+1;
+	int INVALID_MOBILE=1011;
 	
 	/**
 	 * 无效的密码。<br/>
 	 */
-	int INVALID_PASSWORD=INVALID_MOBILE+1;
+	int INVALID_PASSWORD=1012;
 	
 	/**
 	 * 错误的旧密码。<br/>
 	 */
-	int WRONG_OLD_PASSWORD=INVALID_PASSWORD+1;
+	int WRONG_OLD_PASSWORD=1013;
 }
