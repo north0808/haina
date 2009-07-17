6cf279350800ef7555895edd17ecff44474545f5
huangyongqiang <huangyongqiang@6c45ac76-16e4-11de-9d52-39b120432c5d>
2009-07-17 16:32:49 +0000

    修改了查找用户的全部方法

--- haina/codes/beluga/server/beluga/src/com/haina/beluga/dao/ContactUserDao.java
+++ haina/codes/beluga/server/beluga/src/com/haina/beluga/dao/ContactUserDao.java
@@ -2,7 +2,13 @@ package com.haina.beluga.dao;
 
 import java.util.List;
 
+import org.hibernate.criterion.DetachedCriteria;
+import org.hibernate.criterion.Restrictions;
+
+import org.springframework.stereotype.Repository;
+
 import com.haina.beluga.core.dao.BaseDao;
+import com.haina.beluga.core.util.DESUtil;
 import com.haina.beluga.core.util.StringUtils;
 import com.haina.beluga.domain.ContactUser;
 
@@ -14,7 +20,7 @@ import com.haina.beluga.domain.ContactUser;
  * @date 2009-07-03
  */
 
-//@Repository(value="contactUserDao")
+@Repository(value="contactUserDao")
 public class ContactUserDao extends BaseDao<ContactUser,String> implements IContactUserDao {
 
 	@SuppressWarnings("unchecked")
@@ -22,9 +28,13 @@ public class ContactUserDao extends BaseDao<ContactUser,String> implements ICont
 	public ContactUser getValidUserByLoginName(String loginName) {
 		ContactUser contactUser=null;
 		if(!StringUtils.isNull(loginName)) {
-			List<ContactUser> list=this.getHibernateTemplate().findByNamedParam(
-					"from ContactUser u where u.loginName = :loginName and u.validFlag = :validFlag", 
-					new String[]{"loginName","validFlag"}, new Object[]{loginName,Boolean.TRUE});
+			DetachedCriteria detachedCriteria=DetachedCriteria.forClass(ContactUser.class);
+			detachedCriteria.add(Restrictions.and(
+					Restrictions.eq("loginName", loginName), Restrictions.eq("validFlag", Boolean.TRUE)));
+			List<ContactUser> list=this.getUserByHibernateCriteria(detachedCriteria);
+//			this.getHibernateTemplate().findByNamedParam(
+//					"from ContactUser u where u.loginName = :loginName and u.validFlag = :validFlag", 
+//					new String[]{"loginName","validFlag"}, new Object[]{loginName,Boolean.TRUE});
 			if(list!=null && list.size()>0) {
 				contactUser=list.get(0);
 			}
@@ -37,9 +47,13 @@ public class ContactUserDao extends BaseDao<ContactUser,String> implements ICont
 	public ContactUser getValidUserByMobile(String mobile) {
 		ContactUser contactUser=null;
 		if(!StringUtils.isNull(mobile)) {
-			List<ContactUser> list=this.getHibernateTemplate().findByNamedParam(
-					"from ContactUser u where u.mobile = :mobile and u.validFlag = :validFlag", 
-					new String[]{"mobile","validFlag"}, new Object[]{mobile,Boolean.TRUE});
+			DetachedCriteria detachedCriteria=DetachedCriteria.forClass(ContactUser.class);
+			detachedCriteria.add(Restrictions.and(
+					Restrictions.eq("mobile", mobile), Restrictions.eq("validFlag", Boolean.TRUE)));
+			List<ContactUser> list=this.getUserByHibernateCriteria(detachedCriteria);
+//			this.getHibernateTemplate().findByNamedParam(
+//					"from ContactUser u where u.mobile = :mobile and u.validFlag = :validFlag", 
+//					new String[]{"mobile","validFlag"}, new Object[]{mobile,Boolean.TRUE});
 			if(list!=null && list.size()>0) {
 				contactUser=list.get(0);
 			}
@@ -49,13 +63,17 @@ public class ContactUserDao extends BaseDao<ContactUser,String> implements ICont
 
 	@SuppressWarnings("unchecked")
 	@Override
-	public ContactUser getValidUserByLoginNameAndPwd(String loginName,
-			String password) {
+	public ContactUser getValidUserByPwdAndLoginName(String password,String loginName) {
 		ContactUser contactUser=null;
 		if(!StringUtils.isNull(loginName) && !StringUtils.isNull(password)) {
-			List<ContactUser> list=this.getHibernateTemplate().findByNamedParam(
-					"from ContactUser u where u.loginName = :loginName and u.password = :password and u.validFlag = :validFlag", 
-					new String[]{"loginName","password","validFlag"}, new Object[]{loginName,password,Boolean.TRUE});
+			ContactUser exampleUser=new ContactUser();
+			exampleUser.setLoginName(loginName);
+			exampleUser.setPassword(password);
+			exampleUser.setValidFlag(Boolean.TRUE);
+			List<ContactUser> list=this.getUserByExample(exampleUser);
+//			this.getHibernateTemplate().findByNamedParam(
+//					"from ContactUser u where u.loginName = :loginName and u.password = :password and u.validFlag = :validFlag", 
+//					new String[]{"loginName","password","validFlag"}, new Object[]{loginName,password,Boolean.TRUE});
 			if(list!=null && list.size()>0) {
 				contactUser=list.get(0);
 			}
@@ -65,13 +83,17 @@ public class ContactUserDao extends BaseDao<ContactUser,String> implements ICont
 	
 	@SuppressWarnings("unchecked")
 	@Override
-	public ContactUser getInvalidUserByLoginNameAndPwd(String loginName,
-			String password) {
+	public ContactUser getInvalidUserByPwdAndLoginName(String password,String loginName) {
 		ContactUser contactUser=null;
 		if(!StringUtils.isNull(loginName) && !StringUtils.isNull(password)) {
-			List<ContactUser> list=this.getHibernateTemplate().findByNamedParam(
-					"from ContactUser u where u.loginName = :loginName and u.password = :password and u.validFlag = :validFlag", 
-					new String[]{"loginName","password","validFlag"}, new Object[]{loginName,password,Boolean.FALSE});
+			ContactUser exampleUser=new ContactUser();
+			exampleUser.setLoginName(loginName);
+			exampleUser.setPassword(password);
+			exampleUser.setValidFlag(Boolean.FALSE);
+			List<ContactUser> list=this.getUserByExample(exampleUser);
+//			this.getHibernateTemplate().findByNamedParam(
+//					"from ContactUser u where u.loginName = :loginName and u.password = :password and u.validFlag = :validFlag", 
+//					new String[]{"loginName","password","validFlag"}, new Object[]{loginName,password,Boolean.FALSE});
 			if(list!=null && list.size()>0) {
 				contactUser=list.get(0);
 			}
@@ -84,7 +106,7 @@ public class ContactUserDao extends BaseDao<ContactUser,String> implements ICont
 		if(contactUser!=null && !contactUser.isNew() 
 				&& contactUser.getValidFlag()) {
 			contactUser.setValidFlag(Boolean.FALSE);
-			getHibernateTemplate().update(contactUser);
+			this.update(contactUser);
 		}
 	}
 
@@ -93,13 +115,165 @@ public class ContactUserDao extends BaseDao<ContactUser,String> implements ICont
 	public ContactUser getInvalidUserByLoginName(String loginName) {
 		ContactUser contactUser=null;
 		if(!StringUtils.isNull(loginName)) {
-			List<ContactUser> list=this.getHibernateTemplate().findByNamedParam(
-					"from ContactUser u where u.loginName = :loginName and u.validFlag = :validFlag", 
-					new String[]{"loginName","validFlag"}, new Object[]{loginName,Boolean.FALSE});
+			DetachedCriteria detachedCriteria=DetachedCriteria.forClass(ContactUser.class);
+			detachedCriteria.add(Restrictions.and(
+					Restrictions.eq("loginName", loginName), Restrictions.eq("validFlag", Boolean.TRUE)));
+			List<ContactUser> list=this.getUserByHibernateCriteria(detachedCriteria);
+//			this.getHibernateTemplate().findByNamedParam(
+//					"from ContactUser u where u.loginName = :loginName and u.validFlag = :validFlag", 
+//					new String[]{"loginName","validFlag"}, new Object[]{loginName,Boolean.FALSE});
 			if(list!=null && list.size()>0) {
 				contactUser=list.get(0);
 			}
 		}
 		return contactUser;
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public List<ContactUser> getUserByMobileOrLoginName(String mobile, String loginName) {
+		List<ContactUser> list=null;
+		if(!StringUtils.isNull(mobile) &&  !StringUtils.isNull(loginName)) {
+			DetachedCriteria detachedCriteria=DetachedCriteria.forClass(ContactUser.class);
+			detachedCriteria.add(Restrictions.or(
+					Restrictions.eq("mobile", mobile), Restrictions.eq("loginName", loginName)));
+			list=this.getUserByHibernateCriteria(detachedCriteria);
+//			this.getHibernateTemplate().findByNamedParam(
+//					"from ContactUser u where u.loginName = :loginName or u.mobile = :mobile", 
+//					new String[]{"loginName","mobile"}, new Object[]{loginName,mobile});
+		}
+		return list;
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public List<ContactUser> getInvalidUserByMobileOrLoginName(String mobile, String loginName) {
+		List<ContactUser> list=null;
+		if(!StringUtils.isNull(mobile) &&  !StringUtils.isNull(loginName)) {
+			DetachedCriteria detachedCriteria=DetachedCriteria.forClass(ContactUser.class);
+			detachedCriteria.add(Restrictions.and(Restrictions.eq("validFlag", Boolean.FALSE), Restrictions.or(
+					Restrictions.eq("mobile", mobile), Restrictions.eq("loginName", loginName))));
+			list=this.getUserByHibernateCriteria(detachedCriteria);
+		}
+		return list;
+	}
+	
+	@Override
+	public String create(ContactUser o) {
+		return super.create(encryptPassword(o));
+	}
+
+	@Override
+	public ContactUser getBaseModel() {
+		ContactUser contactUser=super.getBaseModel();
+		if(contactUser!=null && !StringUtils.isNull(contactUser.getPassword())) {
+			contactUser.setPassword(DESUtil.decrypt(contactUser.getPassword().trim()));
+		}
+		return contactUser;
+	}
+
+	@Override
+	public List<ContactUser> getModelByPage(ContactUser exampleEntity,
+			int begin, int count) {
+		List<ContactUser> list=super.getModelByPage(this.encryptPassword(exampleEntity), begin, count);
+		list=this.decryptPassword(list);
+		return list;
+	}
+
+	@Override
+	public List<ContactUser> getModels(boolean useCache) {
+		List<ContactUser> list=super.getModels(useCache);
+		list=this.decryptPassword(list);
+		return list;
+	}
+
+	@Override
+	public ContactUser load(String id) {
+		ContactUser contactUser=super.load(id);
+		return this.decryptPassword(contactUser);
+	}
+
+	@Override
+	public ContactUser read(String id) {
+		ContactUser contactUser=super.read(id);
+		return this.decryptPassword(contactUser);
+	}
+
+	@Override
+	public void saveOrUpdate(ContactUser newInstance) {
+		super.saveOrUpdate(encryptPassword(newInstance));
+	}
+
+	@Override
+	public void setBaseModel(ContactUser t) {
+		super.setBaseModel(encryptPassword(t));
+	}
+
+	@Override
+	public void update(ContactUser o) {
+		super.update(encryptPassword(o));
+	}
+	
+	@SuppressWarnings("unchecked")
+	@Override
+	public List<ContactUser> getUserByHibernateCriteria(DetachedCriteria criteria,int begin, int count) {
+		int first=begin;
+		int size=count;
+		if(first<0) {
+			first=1;
+		}
+		if(size<0) {
+			size=1;
+		}
+		List<ContactUser> list=null;
+		if(criteria!=null) {
+			list=this.getModelByHibernateCriteria(criteria, begin, count);
+			list=this.decryptPassword(list);
+		}
+		return list;
+	}
+	
+	@SuppressWarnings("unchecked")
+	@Override
+	public List<ContactUser> getUserByHibernateCriteria(DetachedCriteria criteria) {
+		List<ContactUser> list=null;
+		if(criteria!=null) {
+			list=this.getModelByHibernateCriteria(criteria);
+			list=this.decryptPassword(list);
+		}
+		return list;
+	}
+	
+	private ContactUser encryptPassword(ContactUser contactUser) {
+		ContactUser user=contactUser;
+		if(user!=null && !StringUtils.isNull(user.getPassword())) {
+			user.setPassword(DESUtil.encrypt(user.getPassword().trim()));
+		}
+		return user;
+	}
+	
+	private ContactUser decryptPassword(ContactUser contactUser) {
+		ContactUser user=contactUser;
+		if(user!=null && !StringUtils.isNull(user.getPassword())) {
+			user.setPassword(DESUtil.decrypt(user.getPassword().trim()));
+		}
+		return user;
+	}
+	
+	private List<ContactUser> decryptPassword(List<ContactUser> contactUsers) {
+		List<ContactUser> list=contactUsers;
+		if(list!=null && list.size()>0) {
+			for(int i=0;i<list.size();i++) {
+				list.set(i, this.decryptPassword(list.get(i)));
+			}
+		}
+		return list;
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public List<ContactUser> getUserByExample(ContactUser contactUser) {
+		List<ContactUser> list=this.getHibernateTemplate().findByExample(this.encryptPassword(contactUser));
+		return this.decryptPassword(list);
 	}
 }
