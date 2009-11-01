031778160d9d965767077e8a18b8aa8a52d8954c
north0808@gmail.com <north0808@gmail.com@6c45ac76-16e4-11de-9d52-39b120432c5d>
2009-11-01 03:28:47 +0000

    服务器端增加对电话号段和城市信息表的分页更新支持

--- trunk/haina/codes/beluga/server/beluga/src/com/haina/beluga/contact/service/PhoneDistrictService.java
+++ trunk/haina/codes/beluga/server/beluga/src/com/haina/beluga/contact/service/PhoneDistrictService.java
@@ -11,21 +11,60 @@ import com.haina.beluga.contact.domain.PhoneDistrict;
 import com.haina.beluga.contact.dto.PhoneDistrictDto;
 import com.haina.beluga.webservice.data.hessian.HessianRemoteReturning;
 import com.haina.core.service.BaseSerivce;
+
 @Component
-public class PhoneDistrictService extends BaseSerivce<IPhoneDistrictDao,PhoneDistrict,String> implements IPhoneDistrictService {
+public class PhoneDistrictService extends
+		BaseSerivce<IPhoneDistrictDao, PhoneDistrict, String> implements
+		IPhoneDistrictService {
 
 	@Override
 	public HessianRemoteReturning getOrUpdatePhoneDistricts(int updateFlg) {
 		List<PhoneDistrictDto> list = new ArrayList<PhoneDistrictDto>();
-		Iterator<PhoneDistrict> iterator =getBaseDao().getPhoneDistrictsByUpdateFlg(updateFlg);
-		while(iterator.hasNext()){
+		Iterator<PhoneDistrict> iterator = getBaseDao()
+				.getPhoneDistrictsByUpdateFlg(updateFlg);
+		while (iterator.hasNext()) {
 			PhoneDistrict w = iterator.next();
 			list.add(PhoneDistrictDto.valueof(w));
 		}
 		HessianRemoteReturning hrr = new HessianRemoteReturning(list);
-			hrr.setValue(list);
+		hrr.setValue(list);
 		return hrr;
-		
+
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @seecom.haina.beluga.contact.service.IPhoneDistrictService#
+	 * getOrUpdatePhoneDistricts(int, int, int)
+	 */
+	@Override
+	public HessianRemoteReturning getOrUpdatePhoneDistricts(int updateFlg,
+			int begin, int count) {
+		List<PhoneDistrictDto> list = new ArrayList<PhoneDistrictDto>();
+		Iterator<PhoneDistrict> iterator = getBaseDao()
+				.getPhoneDistrictsByUpdateFlg(updateFlg, begin, count);
+		while (iterator.hasNext()) {
+			PhoneDistrict w = iterator.next();
+			list.add(PhoneDistrictDto.valueof(w));
+		}
+		HessianRemoteReturning hrr = new HessianRemoteReturning(list);
+		hrr.setValue(list);
+		return hrr;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @seecom.haina.beluga.contact.service.IPhoneDistrictService#
+	 * getOrUpdatePhoneDistrictsCount(int)
+	 */
+	@Override
+	public HessianRemoteReturning getOrUpdatePhoneDistrictsCount(int updateFlg) {
+		Long count = getBaseDao().getPhoneDistrictsByUpdateFlgCount(updateFlg);
+		HessianRemoteReturning hrr = new HessianRemoteReturning(count);
+		hrr.setValue(count);
+		return hrr;
 	}
 
 }
