44db3189afe83a9c49caf51c179dc5171ba6e19d
north0808@gmail.com <north0808@gmail.com@6c45ac76-16e4-11de-9d52-39b120432c5d>
2010-05-15 14:19:13 +0000

    C# 客户端网络通讯底层模块测试程序和相关dll库

---
+++ trunk/haina/codes/beluga/client/moblie/BeefWrapNet/BeefWrapTest/FrmMain.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace BeefWrap
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.MainMenu mainMenu1;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mainMenu1 = new System.Windows.Forms.MainMenu();
+            this.menuItemExit = new System.Windows.Forms.MenuItem();
+            this.btnTestSync = new System.Windows.Forms.Button();
+            this.listBoxResult = new System.Windows.Forms.ListBox();
+            this.btnTestASync = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnDownLoad = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.btnUpFile = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // mainMenu1
+            // 
+            this.mainMenu1.MenuItems.Add(this.menuItemExit);
+            // 
+            // menuItemExit
+            // 
+            this.menuItemExit.Text = "退出";
+            this.menuItemExit.Click += new System.EventHandler(this.menuItemExit_Click);
+            // 
+            // btnTestSync
+            // 
+            this.btnTestSync.Location = new System.Drawing.Point(3, 3);
+            this.btnTestSync.Name = "btnTestSync";
+            this.btnTestSync.Size = new System.Drawing.Size(100, 20);
+            this.btnTestSync.TabIndex = 0;
+            this.btnTestSync.Text = "测试同步请求";
+            this.btnTestSync.Click += new System.EventHandler(this.btnTestSync_Click);
+            // 
+            // listBoxResult
+            // 
+            this.listBoxResult.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.listBoxResult.Location = new System.Drawing.Point(0, 63);
+            this.listBoxResult.Name = "listBoxResult";
+            this.listBoxResult.Size = new System.Drawing.Size(240, 198);
+            this.listBoxResult.TabIndex = 1;
+            // 
+            // btnTestASync
+            // 
+            this.btnTestASync.Location = new System.Drawing.Point(109, 3);
+            this.btnTestASync.Name = "btnTestASync";
+            this.btnTestASync.Size = new System.Drawing.Size(100, 20);
+            this.btnTestASync.TabIndex = 2;
+            this.btnTestASync.Text = "测试异步请求";
+            this.btnTestASync.Click += new System.EventHandler(this.btnTestASync_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(157, 29);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(72, 20);
+            this.btnClear.TabIndex = 3;
+            this.btnClear.Text = "清空";
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnDownLoad
+            // 
+            this.btnDownLoad.Location = new System.Drawing.Point(3, 29);
+            this.btnDownLoad.Name = "btnDownLoad";
+            this.btnDownLoad.Size = new System.Drawing.Size(72, 20);
+            this.btnDownLoad.TabIndex = 4;
+            this.btnDownLoad.Text = "下载文件";
+            this.btnDownLoad.Click += new System.EventHandler(this.btnDownLoad_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.progressBar1.Location = new System.Drawing.Point(0, 261);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(240, 7);
+            // 
+            // btnUpFile
+            // 
+            this.btnUpFile.Location = new System.Drawing.Point(79, 29);
+            this.btnUpFile.Name = "btnUpFile";
+            this.btnUpFile.Size = new System.Drawing.Size(72, 20);
+            this.btnUpFile.TabIndex = 5;
+            this.btnUpFile.Text = "上传文件";
+            this.btnUpFile.Click += new System.EventHandler(this.btnUpFile_Click);
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(240, 268);
+            this.Controls.Add(this.btnUpFile);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.btnDownLoad);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnTestASync);
+            this.Controls.Add(this.listBoxResult);
+            this.Controls.Add(this.btnTestSync);
+            this.Menu = this.mainMenu1;
+            this.Name = "FrmMain";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnTestSync;
+        private System.Windows.Forms.ListBox listBoxResult;
+        private System.Windows.Forms.MenuItem menuItemExit;
+        private System.Windows.Forms.Button btnTestASync;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnDownLoad;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button btnUpFile;
+    }
+}
+
---
+++ trunk/haina/codes/beluga/client/moblie/BeefWrapNet/BeefWrapTest/FrmMain.cs
@@ -0,0 +1,377 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Threading;
+using System.Reflection;
+using System.Net;
+using BeefWrap.Net;
+
+namespace BeefWrap
+{
+    public partial class FrmMain : Form
+    {
+        public static readonly string host = "jackyho-acfd27d";
+        string url = "http://" + host + ":8120/BeefWrap";
+
+        bool boolValue = true;
+        float floatValue = 111100.222255F;
+        int intValue = 333399;
+        long longValue = 444455L;
+        string strValue = "你好！";
+        double doubleValue = 666600.777788D;
+
+        #region
+
+        public FrmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void menuItemExit_Click(object sender, EventArgs e)
+        {
+            this.Dispose();
+            Application.Exit();
+        }
+
+        private delegate void DelegateLog(object str);
+        private void Log(object obj)
+        {
+            if (this.InvokeRequired)
+            {
+                this.Invoke(new DelegateLog(this.Log), new object[] { obj });
+                return;
+            }
+            if (null == obj)
+            {
+                this.listBoxResult.Items.Add("null");
+                return;
+            }
+            this.listBoxResult.Items.Add(obj.ToString());
+            Application.DoEvents();
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            this.listBoxResult.Items.Clear();
+        }
+
+        private delegate void DelegateProgress(int value);
+        private void UpdateProgress(int value)
+        {
+            if (this.InvokeRequired)
+            {
+                this.Invoke(new DelegateProgress(this.UpdateProgress), new object[] { value });
+                return;
+            }
+            this.progressBar1.Value = value;
+            Application.DoEvents();
+        }
+
+        #endregion
+
+        #region 同步请求
+        string tokenRW = "";// 标识请求的唯一令牌
+
+        private void btnTestSync_Click(object sender, EventArgs e)
+        {
+            this.btnTestSync.Enabled = false;
+            try
+            {
+                BeefWrapNet net = new BeefWrapNet();
+                this.tokenRW = NetUtility.GetNextToken();
+                // 同步请求
+                net.HttpPostRequest(url + @"/app", this.tokenRW, new RequestPreCallBack(RequestPreRW), new RequestWriteCallBack(RequestWriteRW), new ResponseReadCallBack(ResponseReadRW), new MessageCallBack(MessageRW));
+            }
+            catch { }
+            this.btnTestSync.Enabled = true;
+        }
+
+        private void RequestPreRW(string token, HttpWebRequest request)
+        {
+            if (!this.tokenRW.Equals(token))
+            {
+                throw new Exception("请求已失效");
+            }
+            // 设置请求头
+            request.ContentLength = NetUtility.GetLength(boolValue)
+                + NetUtility.GetLength(floatValue)
+                + NetUtility.GetLength(intValue)
+                + NetUtility.GetLength(longValue)
+                + NetUtility.GetLength(strValue)
+                + NetUtility.GetLength(doubleValue);
+        }
+
+        private void RequestWriteRW(string token, ref Stream stream)
+        {
+            if (!this.tokenRW.Equals(token))
+            {
+                throw new Exception("请求已失效");
+            }
+            // 写基本类型
+            this.Log("写入：" + NetWriter.WriteBool(ref stream, boolValue));
+            this.Log("写入：" + NetWriter.WriteFloat(ref stream, floatValue));
+            this.Log("写入：" + NetWriter.WriteInt(ref stream, intValue));
+            this.Log("写入：" + NetWriter.WriteLong(ref stream, longValue));
+            this.Log("写入：" + NetWriter.WriteUTF(ref stream, strValue));
+            this.Log("写入：" + NetWriter.WriteDouble(ref stream, doubleValue));
+        }
+
+        private void ResponseReadRW(string token, ref Stream stream, HttpWebResponse response)
+        {
+            if (!this.tokenRW.Equals(token))
+            {
+                throw new Exception("请求已失效");
+            }
+            // 读基本类型
+            this.Log("读取：" + NetReader.ReadBool(ref stream));
+            this.Log("读取：" + NetReader.ReadFloat(ref stream));
+            this.Log("读取：" + NetReader.ReadInt(ref stream));
+            this.Log("读取：" + NetReader.ReadLong(ref stream));
+            this.Log("读取：" + NetReader.ReadUTF(ref stream));
+            this.Log("读取：" + NetReader.ReadDouble(ref stream));
+        }
+
+        private void MessageRW(string token, string message, int code)
+        {
+            if (!this.tokenRW.Equals(token))
+            {
+                throw new Exception("请求已失效");
+            }
+            // 请求状态信息
+            this.Log("code = " + code + ",message = " + message);
+        }
+
+        #endregion
+
+        #region 异步请求
+
+        string tokenRWAsync = "";// 标识请求的唯一令牌
+
+        private void btnTestASync_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                BeefWrapNet net = new BeefWrapNet();
+                this.tokenRWAsync = NetUtility.GetNextToken();
+                // 异步请求
+                net.HttpPostRequestAsync(url + @"/app", this.tokenRWAsync, new RequestPreCallBack(RequestPreRWAsync), new RequestWriteCallBack(RequestWriteRWAsync), new ResponseReadCallBack(ResponseReadRWAsync), new MessageCallBack(MessageRWAsync));
+            }
+            catch { }
+        }
+
+        private void RequestPreRWAsync(string token, HttpWebRequest request)
+        {
+            if (!this.tokenRWAsync.Equals(token))
+            {
+                throw new Exception("请求已失效");
+            }
+            // 设置请求头
+            request.ContentLength = NetUtility.GetLength(boolValue)
+                + NetUtility.GetLength(floatValue)
+                + NetUtility.GetLength(intValue)
+                + NetUtility.GetLength(longValue)
+                + NetUtility.GetLength(strValue)
+                + NetUtility.GetLength(doubleValue);
+        }
+
+        private void RequestWriteRWAsync(string token, ref Stream stream)
+        {
+            if (!this.tokenRWAsync.Equals(token))
+            {
+                throw new Exception("请求已失效");
+            }
+            // 写基本类型
+            this.Log("写入：" + NetWriter.WriteBool(ref stream, boolValue));
+            this.Log("写入：" + NetWriter.WriteFloat(ref stream, floatValue));
+            this.Log("写入：" + NetWriter.WriteInt(ref stream, intValue));
+            this.Log("写入：" + NetWriter.WriteLong(ref stream, longValue));
+            this.Log("写入：" + NetWriter.WriteUTF(ref stream, strValue));
+            this.Log("写入：" + NetWriter.WriteDouble(ref stream, doubleValue));
+        }
+
+        private void ResponseReadRWAsync(string token, ref Stream stream, HttpWebResponse response)
+        {
+            if (!this.tokenRWAsync.Equals(token))
+            {
+                throw new Exception("请求已失效");
+            }
+            // 读基本类型
+            this.Log("读取：" + NetReader.ReadBool(ref stream));
+            this.Log("读取：" + NetReader.ReadFloat(ref stream));
+            this.Log("读取：" + NetReader.ReadInt(ref stream));
+            this.Log("读取：" + NetReader.ReadLong(ref stream));
+            this.Log("读取：" + NetReader.ReadUTF(ref stream));
+            this.Log("读取：" + NetReader.ReadDouble(ref stream));
+        }
+
+        private void MessageRWAsync(string token, string message, int code)
+        {
+            if (!this.tokenRWAsync.Equals(token))
+            {
+                throw new Exception("请求已失效");
+            }
+            // 请求状态信息
+            this.Log("code = " + code + ",message = " + message);
+        }
+
+        #endregion
+
+        #region 下载
+
+        string picUrl = "http://" + host + ":8120/BeefWrap/down.jpg";
+        string tokenDownLoad = "";// 标识下载的唯一令牌
+
+        private void btnDownLoad_Click(object sender, EventArgs e)
+        {
+            this.btnDownLoad.Enabled = false;
+            this.progressBar1.Value = 0;
+            try
+            {
+                BeefWrapNet net = new BeefWrapNet();
+                this.tokenDownLoad = NetUtility.GetNextToken();
+                // 异步下载
+                net.HttpGetRequestAsync(picUrl, this.tokenDownLoad, null, null, new ResponseReadCallBack(ResponseReadDownLoad), new MessageCallBack(MessageDownLoad));
+            }
+            catch { }
+            this.btnDownLoad.Enabled = true;
+        }
+
+        private void RequestPreDownLoad(string token, HttpWebRequest request)
+        {
+            if (!this.tokenDownLoad.Equals(token))
+            {
+                throw new Exception("请求已失效");
+            }
+            // 设置请求头
+            request.ContentLength = NetUtility.GetLength(boolValue)
+                + NetUtility.GetLength(floatValue)
+                + NetUtility.GetLength(intValue)
+                + NetUtility.GetLength(longValue)
+                + NetUtility.GetLength(strValue)
+                + NetUtility.GetLength(doubleValue);
+        }
+
+        private void ResponseReadDownLoad(string token, ref Stream stream, HttpWebResponse response)
+        {
+            if (!this.tokenDownLoad.Equals(token))
+            {
+                throw new Exception("请求已失效");
+            }
+            // 文件下载
+            string path = Path.GetDirectoryName(Assembly.GetExecutingAssembly().GetName().CodeBase);
+            byte[] bytes = new byte[1024];
+            long pos = 0;
+            FileStream fs = new FileStream(path + @"\" + picUrl.Substring(picUrl.LastIndexOf("/") + 1), FileMode.Create, FileAccess.Write);
+            this.Log("开始下载...");
+            while (true)
+            {
+                if (!this.tokenDownLoad.Equals(token))
+                {
+                    throw new Exception("请求已失效");
+                }
+                int len = stream.Read(bytes, 0, bytes.Length);
+                if (len <= 0)
+                {
+                    break;
+                }
+                pos += len;
+                fs.Write(bytes, 0, len);
+                fs.Flush();
+                this.UpdateProgress((int)((pos * 100) / response.ContentLength));
+            }
+            fs.Close();
+        }
+
+        private void MessageDownLoad(string token, string message, int code)
+        {
+            if (!this.tokenDownLoad.Equals(token))
+            {
+                throw new Exception("请求已失效");
+            }
+            // 请求状态信息
+            this.Log("code = " + code + ",message = " + message);
+        }
+
+        #endregion
+
+        #region 上传文件
+
+        string upUrl = "http://" + host + ":8120/BeefWrap/up";
+        string tokenUp = "";// 标识下载的唯一令牌
+
+        private void btnUpFile_Click(object sender, EventArgs e)
+        {
+            this.btnUpFile.Enabled = false;
+            this.progressBar1.Value = 0;
+            try
+            {
+                BeefWrapNet net = new BeefWrapNet();
+                this.tokenUp = NetUtility.GetNextToken();
+                // 异步上传
+                net.HttpPostRequestAsync(upUrl, this.tokenUp, new RequestPreCallBack(RequestPreUp), new RequestWriteCallBack(RequestWriteUp), null, new MessageCallBack(MessageUp));
+            }
+            catch { }
+            this.btnUpFile.Enabled = true;
+        }
+
+        private void RequestPreUp(string token, HttpWebRequest request)
+        {
+            if (!this.tokenUp.Equals(token))
+            {
+                throw new Exception("请求已失效");
+            }
+            // TODO:设置请求头
+            string path = Path.GetDirectoryName(Assembly.GetExecutingAssembly().GetName().CodeBase);
+            FileInfo fileInfo = new FileInfo(path + @"\" + picUrl.Substring(picUrl.LastIndexOf("/") + 1));
+            request.ContentLength = fileInfo.Length;
+        }
+
+        private void RequestWriteUp(string token, ref Stream stream)
+        {
+            if (!this.tokenUp.Equals(token))
+            {
+                throw new Exception("请求已失效");
+            }
+            // 文件下载
+            string path = Path.GetDirectoryName(Assembly.GetExecutingAssembly().GetName().CodeBase);
+            byte[] bytes = new byte[1024];
+            long pos = 0;
+            FileStream fs = new FileStream(path + @"\" + picUrl.Substring(picUrl.LastIndexOf("/") + 1), FileMode.Open, FileAccess.Read);
+            FileInfo fileInfo = new FileInfo(path + @"\" + picUrl.Substring(picUrl.LastIndexOf("/") + 1));
+            this.Log("开始上传...");
+            while (true)
+            {
+                if (!this.tokenUp.Equals(token))
+                {
+                    throw new Exception("请求已失效");
+                }
+                int len = fs.Read(bytes, 0, bytes.Length);
+                if (len <= 0)
+                {
+                    break;
+                }
+                pos += len;
+                stream.Write(bytes, 0, len);
+                this.UpdateProgress((int)((pos * 100) / fileInfo.Length));
+            }
+            fs.Close();
+        }
+
+        private void MessageUp(string token, string message, int code)
+        {
+            if (!this.tokenUp.Equals(token))
+            {
+                throw new Exception("请求已失效");
+            }
+            // 请求状态信息
+            this.Log("code = " + code + ",message = " + message);
+        }
+
+        #endregion
+    }
+}
