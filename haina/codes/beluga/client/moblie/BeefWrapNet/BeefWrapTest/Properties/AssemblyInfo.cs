e756a4f4d18143f1586f18c4fb3f7d54ff9cdbaf
north0808 <north0808@gmail.com>
2010-05-15 14:19:13 +0000

    C# 客户端网络通讯底层模块测试程序和相关dll库

---
+++ haina/codes/beluga/client/moblie/BeefWrapNet/BeefWrapTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// 有关程序集的常规信息通过下列属性集
+// 控制。更改这些属性值可修改
+// 与程序集关联的信息。
+[assembly: AssemblyTitle("BeefWrapTest")]
+[assembly: AssemblyDescription("C# 客户端网络通讯底层模块测试程序")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("牛五方")]
+[assembly: AssemblyProduct("BeefWrapTest")]
+[assembly: AssemblyCopyright("Copyright © 牛五方 2010")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// 将 ComVisible 设置为 false 使此程序集中的类型
+// 对 COM 组件不可见。如果需要从 COM 访问此程序集中的类型，
+// 则将该类型上的 ComVisible 属性设置为 true。
+[assembly: ComVisible(false)]
+
+// 如果此项目向 COM 公开，则下列 GUID 用于类型库的 ID
+[assembly: Guid("a0dcf4c4-ba51-4831-a1ba-769662d49793")]
+
+// 程序集的版本信息由下面四个值组成:
+//
+//      主版本
+//      次版本
+//      内部版本号
+//      修订号
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+
+// 以下属性会取消显示 FxCop 警告“CA2232 : Microsoft.Usage : 向程序集添加 STAThreadAttribute 属性”，
+// 因为设备应用程序不支持 STA 线程。
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2232:MarkWindowsFormsEntryPointsWithStaThread")]
