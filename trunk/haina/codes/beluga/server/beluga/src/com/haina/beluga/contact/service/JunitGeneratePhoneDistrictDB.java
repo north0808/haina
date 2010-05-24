19d3efc9881f4fce0116be31a3ffd8e64fe567e1
huangyongqiang <huangyongqiang@6c45ac76-16e4-11de-9d52-39b120432c5d>
2010-05-24 15:52:51 +0000

    根据FileUtil的调整修改了测试方法

--- trunk/haina/codes/beluga/server/beluga/src/com/haina/beluga/contact/service/JunitGeneratePhoneDistrictDB.java
+++ trunk/haina/codes/beluga/server/beluga/src/com/haina/beluga/contact/service/JunitGeneratePhoneDistrictDB.java
@@ -113,7 +113,7 @@ public class JunitGeneratePhoneDistrictDB extends TestCase
 				//e1.printStackTrace();
 			}
 			System.out.println("reload:"+phoneCode);
-			FileUtil.write("F:\\124.txt", "reload:"+phoneCode);
+			FileUtil.writeFile("F:\\124.txt", "reload:"+phoneCode, true);
 			return getStrByNet(phoneCode);
 			 
 		}
