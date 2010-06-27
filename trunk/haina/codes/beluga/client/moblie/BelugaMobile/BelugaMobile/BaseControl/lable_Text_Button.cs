85bc33ed3fd3bbbb3ab4bebaf1bb8c7f05ec5585
luweishun@gmail.com <luweishun@gmail.com@6c45ac76-16e4-11de-9d52-39b120432c5d>
2010-06-27 01:42:32 +0000

    输入法页面

---
+++ trunk/haina/codes/beluga/client/moblie/BelugaMobile/BelugaMobile/BaseControl/lable_Text_Button.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Text;
+using System.Windows.Forms;
+
+namespace BelugaMobile.BaseControl
+{
+    public partial class lable_Text_Button : UserControl
+    {
+        public lable_Text_Button()
+        {
+            InitializeComponent();
+        }
+
+        public string LableText 
+        {
+            get 
+            {
+                return this.lable.Text;
+            }
+            set 
+            {
+                this.lable.Text = value;
+            }
+        }
+
+        public string TextBoxText
+        {
+            get
+            {
+                return this.textBox.Text;
+            }
+            set
+            {
+                this.textBox.Text = value;
+            }
+        }
+    }
+}
